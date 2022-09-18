7c692a281f433453c2b1ee5089d7305fb4f8c3f3
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-09-18 15:59:51 +0300

    Task 4 solved

--- Lesson_5/Task4.py
+++ Lesson_5/Task4.py
@@ -1,1 +1,18 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+text = input()
+count = 1
+result = []
+for i in range(len(text) - 1):
+    if text[i] == text[i + 1]:
+        count += 1
+    else:
+        result.append((text[i],count))
+        count = 1
+result.append((text[i+1],count))
+print(result)
+
+rehab = ''
+for j in range(len(result)):
+    rehab += result[j][0] * result[j][1]
+print(rehab)
